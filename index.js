07d928e5688e7cc0f4a3d969a1a06e5b96f4c9b3
Ivan Borisenko <avesus8@gmail.com>
2016-06-30 02:24:35 -0700

    Refactoring to better code

--- index.js
+++ index.js
@@ -1,83 +1,68 @@
-// TODO: split functionality to building, completion notifying
-// and watching. With support of production builds without watches
-// and reload handlers.
-// Make all functionality available via API instead of running
-// a process. Make possibility to supply resulting bundle
-// in memory instead of thru a file.
+// TODO: split functionality to building, completion notifying and watching.
+//
+// With support of production builds without watches and reload handlers.
+//
+// Make all functionality available via API instead of running a process. 
+//
+// Make possibility to supply resulting bundle in memory instead of thru a file.
+// 
 // Separate index.html bundling from core bundling,
 // allow externalize that functionality
 // (but support thru specifying source map offset -
 // position of JavaScript <script> code in HTML).
-// Remove reloading support into example, but make use
-// of API.
+// 
+// Remove reloading support into example, but make use of API.
+// 
 // Use caching to prevent re-reading of files if nothing changed,
-// only track changed files.
+// only track changed files. Use parser both to count lines
+// and to find lines of code containing require() calls.
+// Comparing added / removed require() calls with
+// previous version of file make use of complete rebuild.
+//
 // Build and include in bundled map the source map for the
 // browser require script require-script.js.
 
-var argv = require('optimist').argv;
-
-if (!argv.entry) {
-  console.log('usage: bla --entry index.js');
-  process.exit(0);
-}
-
-var entryPath = process.cwd() + '/' + argv.entry;
-
 var path = require('path');
 var chokidar = require('chokidar');
-var combineSourceMap = require('combine-source-map');
 var fs = require('fs');
 
 var depsTree = require('./lib/deps-tree');
+var buildBundleJs = require('./lib/build-bundle');
 
-var browserRequireScript = fs.readFileSync(__dirname + '/browser-scripts/require-script.js');
+// TODO: make support of output file, bundling type (html, separate js), source maps separation,
+// hosing type (self-hosted, external, file://)
+if (require.main === module) {
+  var argv = require('optimist').argv;
 
-var watcher = chokidar.watch([], {persistent: true})
-  .on('change', buildAll)
-  .on('unlink', buildAll)
+  if (!argv._[0]) {
+    console.log('usage: node ./node_modules/deps-bundler --watch --output index.html index.js');
+    process.exit(0);
+  }
 
+  var entryPath = process.cwd() + '/' + (argv._[0] || 'index.js');
+  var outputPath = process.cwd() + '/' + (argv.output || 'index.html');
 
-var watchFileList = [];
-
-var promiseRef = undefined;
-
-var g_ws = undefined;
-
-function buildAll () {
-  console.log('Rebuilding...');
-  watcher.unwatch(watchFileList);
-
-  depsTree(entryPath)
-    .then(function (deps) {
-
-
-      build(deps);
-
-      if (g_ws) {
-        g_ws.send('reload');
-      }
-
-      console.log('Rebuild complete.');
-
-      watcher.add(deps.files);
-      watchFileList = deps.files;
-    })
-    .catch(function (err) {
-      console.log('Error', err);
+  if (!argv.watch) {
+    bundle(entryPath, 0, function onBuildComplete (scriptBody, sourceMap) {
+      console.log(scriptBody);
+      console.log(sourceMap.comment());
     });
+  } else {
+    autoReloadServer(entryPath, outputPath);
+  }
 }
 
-buildAll();
 
-function build (deps) {
-
-
-var source = fs.readFileSync(entryPath, 'utf-8');
-
-var sourceMap = combineSourceMap.create();
-
-function lineBreakCount (str){
+// TODO:
+// this function should:
+//   a) calculate count of lines in source file;
+//   b) strip source map comment;
+//   c) detect lines changes containing require() calls.
+// For the last case, this function should be supplied with
+// an array of previous lines containing require() calls
+// and output such array for caching.
+// TODO 2: remove this function to file
+function lineBreakCount (str) {
   try {
     return((str.match(/[^\n]*\n[^\n]*/gi).length));
   } catch(e) {
@@ -85,60 +70,95 @@ function lineBreakCount (str){
   }
 }
 
-var combinedHtml = '<html><head><script>var modulesInfo = {\n';
+module.exports.bundle = bundle;
+function bundle (entryPath, sourceMapShiftLines, onBuildComplete) {
 
-function addFile (depsData, sinceLine) {
-  var linesAdded = 0;
+  depsTree(entryPath)
+    .then(function (deps) {
 
-  sourceMap.addFile({ sourceFile: depsData.file, source: depsData.source },
-    { line: sinceLine + 1 });
+      for (var fileIndex = 0; fileIndex < deps.order.length; ++fileIndex) {
+        // Uncomment to debug module line numbers
+        // console.log(sinceLine + 2);
+        var moduleDesc = deps.deps[deps.order[fileIndex]];
 
-  var source = depsData.source.substring(0, depsData.source.lastIndexOf("\n"));
+        // Strip source map comments
+        // TODO: move this to lineBreakCount function along with require() calls matcher.
+        moduleDesc.stripedSource = moduleDesc.source.substring(0, moduleDesc.source.lastIndexOf("\n"));
 
-  linesAdded += lineBreakCount(source);
-  combinedHtml += "'" + depsData.file + "': [" + JSON.stringify(depsData.deps) +
-    ", function (exports, require, module, __filename, __dirname) {\n";
-  combinedHtml += source;
-  combinedHtml += "\n}],\n";
+        moduleDesc.lineBreakCount = lineBreakCount(moduleDesc.stripedSource);
+      }
 
-  return linesAdded + 3;
+      // TODO: use streams and put module-by-module.
+      // On stream's end write require() code for browser.
+      var result = buildBundleJs(entryPath, deps, sourceMapShiftLines);
+
+      onBuildComplete(result.scriptBody, result.sourceMap, deps.files);
+
+    })
+    .catch(function (err) {
+      console.log('Error', err);
+    });
+};
+
+
+module.exports.bundleWatch = function bundleWatch (entryPath, sourceMapShiftLines, onBuildComplete) {
+
+  var watcher = chokidar.watch([], {persistent: true})
+    .on('change', rebuild)
+    .on('unlink', rebuild)
+
+  var watchFileList = [];
+
+  function rebuild () {
+
+    console.log('Rebuilding...');
+    watcher.unwatch(watchFileList);
+
+    bundle(entryPath, sourceMapShiftLines, function onBuildCompleteWatch (scriptBody, sourceMap, allFiles) {
+        // TODO: instead of allFiles, supply
+        // with added and removed files.
+
+        console.log('Rebuild complete.');
+
+        onBuildComplete(scriptBody, sourceMap);
+
+        watcher.add(allFiles);
+        watchFileList = allFiles;
+      });
+  }
+
+  rebuild();
+};
+
+
+// TODO: make support of agile changing url/port/HTML template and include way etc.
+function autoReloadServer (entryPath, outputPath) {
+  var g_ws = undefined;
+
+  var htmlScriptOffsetLine = 0;
+  function bundleIntoHtml (scriptBody, sourceMap) {
+    // sourceMap.base64() - source map file ready to serve.
+    fs.writeFileSync(outputPath, '<html><head><script>' + scriptBody + '\n' + sourceMap.comment() + '</script><style>' +
+    // fs.readFileSync('./qp.css', 'utf-8') + 
+    '</style></head><body><div id="container"></div></body></html>', 'utf-8');
+  }
+
+  bundleWatch(entryPath, htmlScriptOffsetLine, function onRebuilt (scriptBody, sourceMap) {
+    bundleIntoHtml(scriptBody, sourceMap);
+
+    if (g_ws) {
+      g_ws.send('reload');
+    }
+  });
+
+  var WebSocketServer = require('ws').Server;
+  var wss = new WebSocketServer({ port: 8080 });
+
+  wss.on('connection', function connection (ws) {
+    console.log('Debugging browser connected.');
+
+    g_ws = ws;
+
+  });
 }
-
-
-var sinceLine = 1;
-
-for (var fileIndex = 0; fileIndex < deps.order.length; ++fileIndex) {
-  console.log(sinceLine + 2);
-  sinceLine += addFile(deps.deps[deps.order[fileIndex]], sinceLine);
-}
-
-combinedHtml += "};\n";
-
-combinedHtml += browserRequireScript;
-
-console.log(sinceLine);
-
-fs.writeFileSync('./index.html', combinedHtml + '\n' + sourceMap.comment() + '</script><style>' +
-  // fs.readFileSync('./qp.css', 'utf-8') + 
-  '</style></head><body><div id="container"></div><script>' +
-  "document.body.onload = function () { loadModule('" + entryPath + "');};" +
-  '</script></body></html>', 'utf-8');
-}
-
-
-
-
-
-var ws = require('ws');
-
-var WebSocketServer = require('ws').Server
-  , wss = new WebSocketServer({ port: 8080 });
-
-wss.on('connection', function connection(ws) {
-  console.log('Debugging browser connected.');
-
-  g_ws = ws;
-
-  ws.send('something');
-});
 
